34de64f11e591efdb86c0158c47da40b314d4b7e
Luke Mico <lukemico@me.com>
2019-05-01 21:39:38 +1000

    this should fail

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render } from 'react-testing-library';
 import App from './App';
+import { exportAllDeclaration } from '@babel/types';
 
 describe('App', () => {
   it('Renders without error', () => {
     render(<App />);
+
+    it('Fails', () => {
+      expect(true).toBe(false);
+    });
   });
 });
