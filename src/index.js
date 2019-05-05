950b5dfdf6c93de278593667bb7e303b542de342
Luke Mico <lukemico@me.com>
2019-05-05 10:38:00 +1000

    Create an Error Boundary for a React App

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '@babel/polyfill';
 import App from './App';
+import DefaultErrorBoundary from './DefaultErrorBoundary';
 import './styles.css';
 
 ReactDOM.render(
   // wrap App component with React.StrictMode to highlight and avoid any deprecated APIs
   <React.StrictMode>
-    <App />
+    <DefaultErrorBoundary>
+      <App />
+    </DefaultErrorBoundary>
   </React.StrictMode>,
   document.getElementById('app')
 );
