9e0d3e4e9c63aa4c45467fd74a5311ec3db71977
luke <luke@bitscan.co>
2019-05-20 16:08:05 +1000

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
 import { BrowserRouter } from 'react-router-dom';
-import AppRoutes from './App';
+import AppRoutes from '../src/App/routes';
 
 class App extends React.Component {
   // definte the state for count
--- src/App/routes.js
+++ src/App/routes.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Home from './scenes/home';
-import Transfers from './scenes/transfers';
-import Transactions from './scenes/transactions';
-import Contact from './scenes/contact';
+import Home from '../scenes/home';
+import Transfers from '../scenes/transfers';
+import Transactions from '../scenes/transactions';
+import Contact from '../scenes/contact';
 
 // this exports the AppRoutes variable using React-Router <Switch> component, which renders first child <Route> that matches location.
 export const AppRoutes = () => {
